d94faed448ed04f25a052e813cd4de3ed2d0614e
sergey.s.betke <sergey.s.betke@yandex.ru>
2017-09-21 18:39:08 +0300

    * добавлены скрипты для подключения ITG.MakeUtils (#11)

---
+++ tools/add-itg.makeutils.cmd
@@ -0,0 +1,2 @@
+git remote add makeutils git@github.com:IT-Service/ITG.MakeUtils.git
+git subtree add --prefix=ITG.MakeUtils --squash makeutils master
---
+++ tools/update-itg.makeutils.cmd
@@ -0,0 +1,1 @@
+git subtree pull --prefix=ITG.MakeUtils --squash makeutils master
