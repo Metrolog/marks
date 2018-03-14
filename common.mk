b4000f446d6e33193f8a73a71bf261b807431366
Sergey S. Betke <sergey.s.betke@yandex.ru>
2018-03-15 01:12:24 +0300

    * отказался от использования mkdir в пользу powershell (#9)

--- common.mk
+++ common.mk
@@ -22,52 +22,6 @@ LEFT_BRACKET       :=(
 RIGHT_BRACKET      :=)
 DOLLAR_SIGN        :=$$
 
-ifeq ($(OS),Windows_NT)
-
-PATHSEP            :=;
-MAKETARGETDIR      = /usr/bin/mkdir -p $(@D)
-MAKETARGETASDIR    = /usr/bin/mkdir -p $@
-
-else
-
-PATHSEP            :=:
-MAKETARGETDIR      = mkdir -p $(@D)
-MAKETARGETASDIR    = mkdir -p $@
-
-endif
-
-ZIP                ?= zip \
-	-o \
-	-9
-TAR                ?= tar
-
-# $(call setvariable, var, value)
-define setvariable
-$1:=$2
-
-endef
-
-# $(call copyfile, to, from)
-define copyfile
-$1: $2
-	$$(MAKETARGETDIR)
-	cp $$< $$@
-endef
-
-# $(call copyfileto, todir, fromfile)
-copyfileto = $(call copyfile,$1/$(notdir $2),$2)
-
-# $(call copyfilefrom, tofile, fromdir)
-copyfilefrom = $(call copyfile,$1,$2/$(notdir $1))
-
-# $(call copyFilesToZIP, targetZIP, sourceFiles, sourceFilesRootDir)
-define copyFilesToZIP
-$1:$2
-	$$(MAKETARGETDIR)
-	cd $3 && $(ZIP) -FS -o -r -D $$(abspath $$@) $$(patsubst $3/%, %, $$^)
-	@touch $$@
-endef
-
 # $(call winPath,sourcePathOrFileName)
 winPath = $(shell cygpath -w $1)
 
@@ -103,6 +57,63 @@ psExecuteCommand = \
     -Command "\
       Set-Variable -Name ErrorActionPreference -Value Stop; \
       & { $(1) }"
+
+ifeq ($(OS),Windows_NT)
+
+SHELL              := cmd
+.SHELLFLAGS        := /c
+
+PATHSEP            :=;
+MKDIR              = \
+	$(PowerShell) \
+		-File $(call OSPath,$(MAKE_COMMON_DIR)/mkdir.ps1) \
+		-p \
+		-Verbose
+
+else
+
+SHELL              := /bin/sh
+.SHELLFLAGS        := -c
+
+PATHSEP            :=:
+MKDIR              = mkdir -p
+
+endif
+
+MAKETARGETDIR      = $(MKDIR) $(@D)
+MAKETARGETASDIR    = $(MKDIR) $@
+
+ZIP                ?= zip \
+	-o \
+	-9
+TAR                ?= tar
+
+# $(call setvariable, var, value)
+define setvariable
+$1:=$2
+
+endef
+
+# $(call copyfile, to, from)
+define copyfile
+$1: $2
+	$$(MAKETARGETDIR)
+	cp $$< $$@
+endef
+
+# $(call copyfileto, todir, fromfile)
+copyfileto = $(call copyfile,$1/$(notdir $2),$2)
+
+# $(call copyfilefrom, tofile, fromdir)
+copyfilefrom = $(call copyfile,$1,$2/$(notdir $1))
+
+# $(call copyFilesToZIP, targetZIP, sourceFiles, sourceFilesRootDir)
+define copyFilesToZIP
+$1:$2
+	$$(MAKETARGETDIR)
+	cd $3 && $(ZIP) -FS -o -r -D $$(abspath $$@) $$(patsubst $3/%, %, $$^)
+	@touch $$@
+endef
 
 #
 # subprojects
