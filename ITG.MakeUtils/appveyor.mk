b8f85c654c56b7ca7f4b06dc0d8565075a080e5c
Sergey S. Betke <sergey.s.betke@yandex.ru>
2018-05-24 19:50:02 +0300

    * исправляем публикацию артефактов на AppVeyor на PowerShell (#40)

--- ITG.MakeUtils/appveyor.mk
+++ ITG.MakeUtils/appveyor.mk
@@ -14,7 +14,7 @@ APPVEYORTOOL ?= appveyor
 
 pushDeploymentArtifactFile = \
   $(call psExecuteCommand,\
-		'$(2)' -split ' ' | Push-AppveyorArtifact -DeploymentName '$(1)' \
+		'$(2)' -split ' ' | % { Push-AppveyorArtifact -File $$$$_ -DeploymentName '$(1)' } \
   )
 
 pushDeploymentArtifact = $(call pushDeploymentArtifactFile,$@,$^)
