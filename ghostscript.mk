4ac030ecc6306499b7bc37009399f93184e20298
Sergey S. Betke <sergey.s.betke@yandex.ru>
2018-06-08 01:09:34 +0300

    * добавлено общее правило с рецептом в ghostscript.mk для pdf файлов * файлы заказов перенесены в stamps/sources

--- ghostscript.mk
+++ ghostscript.mk
@@ -1,24 +1,32 @@
 ifndef MAKE_GHOSTSCRIPT_DIR
 MAKE_GHOSTSCRIPT_DIR := $(realpath $(dir $(lastword $(MAKEFILE_LIST))))
-ITG_MAKEUTILS_DIR   ?= $(MAKE_GHOSTSCRIPT_DIR)
+ITG_MAKEUTILS_DIR ?= $(MAKE_GHOSTSCRIPT_DIR)
 
 include $(realpath $(ITG_MAKEUTILS_DIR)/common.mk)
 
 ifeq ($(OS),Windows_NT)
 
-GSTOOL             ?= gswin64c
+GSTOOL ?= gswin64c
 
 else
 
-GSTOOL             ?= gs
+GSTOOL ?= gs
 
 endif
 
-GS                 := $(GSTOOL) \
+GS = $(GSTOOL) \
   -dSAFER \
   -dNOPAUSE \
   -dBATCH \
   -sDEVICE=pdfwrite \
   -dNOPLATFONTS
+
+GSINCDIR ?=
+GSFONTDIR ?=
+
+$(OUTPUTDIR)/%.pdf: $(SOURCESDIR)/%.ps
+	$(info Build file "$@" from "$<"...)
+	$(MAKETARGETDIR)
+	$(GS) -sOutputFile='$(call OSPath,$@)' $(foreach incdir,$(GSINCDIR),-I'$(call OSabsPath,$(incdir))') -sFONTPATH='$(call OSabsPath,$(GSFONTDIR))' '$(call OSPath,$<)'
 
 endif
