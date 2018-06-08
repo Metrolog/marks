52abc302cbaa42c87482e7c0babd9680c6065b5b
Sergey S. Betke <sergey.s.betke@yandex.ru>
2018-06-08 03:03:24 +0300

    * косметические правки идентификатора теста (Metrolog/marks#46)

--- ghostscript.mk
+++ ghostscript.mk
@@ -38,7 +38,7 @@ TESTSPDFFILES = $(patsubst $(TESTSDIR)/%.ps,$(AUXDIR)/%.pdf,$(TESTSPSFILES))
 # $(call definePSBuildTest,target,source,dependencies)
 define definePSBuildTest
 
-$(call defineTest,$1,ps_build,\
+$(call defineTest,$(basename $(notdir $1)),ps_build,\
   $$(GS) -sOutputFile='$$(call OSPath,$1)' $$(foreach incdir,$$(GSINCDIR),-I'$$(call OSabsPath,$$(incdir))') -sFONTPATH='$$(call OSabsPath,$(GSFONTDIR))' '$$(call OSPath,$2)';,\
   $2 $3 \
 )
