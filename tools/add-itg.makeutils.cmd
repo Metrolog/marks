1eac8d3971f613742ce1e1f40405f36d3461226f
Sergey S. Betke <sergey.s.betke@yandex.ru>
2018-06-17 01:49:18 +0300

    * убран  `--squash` при обработке ITG.MakeUtils полностью

--- tools/add-itg.makeutils.cmd
+++ tools/add-itg.makeutils.cmd
@@ -1,2 +1,2 @@
 git remote add makeutils git@github.com:IT-Service/ITG.MakeUtils.git
-git subtree add --prefix=ITG.MakeUtils --squash makeutils master
+git subtree add --prefix=ITG.MakeUtils makeutils master
