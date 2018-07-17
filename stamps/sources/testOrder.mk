3fd8f0b18dad3fcf90a1323e574aa802adab7430
Sergey S. Betke <sergey.s.betke@yandex.ru>
2018-07-18 00:34:06 +0300

    * добавлена тестовая генерация выходных файлов для мультифайлового режима (#69)

--- stamps/sources/testOrder.mk
+++ stamps/sources/testOrder.mk
@@ -4,11 +4,12 @@ ifndef STAMPS_MAKEFILE_LOADED
 $(error Do not run this file directly. Run 'Makefile' in root project folder.)
 endif
 
-$(call create_verification_stamps, \
+$(eval $(call create_verification_stamps, \
 	2018, \
 	$(call months,$(call range,1,12)) $(call quarters,$(call range,1,4)) $(call year), \
 	СП, \
 	$(call range,1,2), \
 	18 mm, \
-	false \
-)
+	false, \
+	$(lastword $(MAKEFILE_LIST)) \
+))
