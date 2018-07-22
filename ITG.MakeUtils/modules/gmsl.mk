fbae30a94ec6d858a68490723c7edeee56231b48
Sergey S. Betke <sergey.s.betke@yandex.ru>
2018-07-22 15:31:52 +0300

    * изменено подключение GMSL (IT-Service/ITG.MakeUtils#16) [ci skip]

--- ITG.MakeUtils/modules/gmsl.mk
+++ ITG.MakeUtils/modules/gmsl.mk
@@ -1,21 +1,10 @@
 #!/usr/bin/make
 
-ITG_MAKEUTILS_DIR ?= ITG.MakeUtils/
 ROOT_PROJECT_DIR ?= ./
-REPOROOT = $(ROOT_PROJECT_DIR)
-GSMLREPO ?= git://git.code.sf.net/p/gmsl/gmsl
-GMSLDIR ?= $(ITG_MAKEUTILS_DIR)GSML/
+ITG_MAKEUTILS_DIR ?= $(ROOT_PROJECT_DIR)ITG.MakeUtils/
+include $(ITG_MAKEUTILS_DIR)common.mk
+include $(ITG_MAKEUTILS_DIR)git.mk
 
-GIT ?= git
+# sub projects
 
-.PHONY: add-gmsl
-add-gmsl: $(GMSLDIR)gmsl
-
-$(GMSLDIR)gmsl:
-	if   $(GIT) remote | grep gmsl  $(GIT) remote add gmsl -f --no-tags --mirror=fetch $(GSMLREPO)
-	$(GIT) subtree add --prefix=$(GMSLDIR) --squash gmsl master
-	-$(GIT) remote remove gmsl
-
-.PHONY: update-gmsl
-update-gmsl: $(GMSLDIR)gmsl
-	$(GIT) subtree pull --prefix=$(GMSLDIR) --squash $(GSMLREPO) master
+$(eval $(call useExternalSubProjectAsSubtree,gmsl,git://git.code.sf.net/p/gmsl/gmsl,$(ITG_MAKEUTILS_DIR)GMSL/))
