fa09f5b9b3298ce36365fee24941992b603e67b2
Sergey S. Betke <sergey.s.betke@yandex.ru>
2018-07-22 11:46:19 +0300

    * добавлена заготовка сценария для подключения GMSL (IT-Service/ITG.MakeUtils#16) [ci skip]

---
+++ ITG.MakeUtils/modules/gmsl.mk
@@ -0,0 +1,23 @@
+#!/usr/bin/make
+
+ITG_MAKEUTILS_DIR ?= ITG.MakeUtils/
+ROOT_PROJECT_DIR ?= ./
+REPOROOT = $(ROOT_PROJECT_DIR)
+GSMLREPO ?= git://git.code.sf.net/p/gmsl/gmsl
+GMSLDIR ?= $(ITG_MAKEUTILS_DIR)GSML/
+
+GIT ?= git
+
+.PHONY: add-gmsl
+add-gmsl: $(GMSLDIR)gmsl
+
+$(GMSLDIR)gmsl:
+	-$(GIT) remote add gmsl $(GSMLREPO)
+	$(GIT) subtree add --prefix=$(GMSLDIR) --squash gmsl master
+	-$(GIT) remote remove gmsl
+
+.PHONY: update-gmsl
+update-gmsl: $(GMSLDIR)gmsl
+	-$(GIT) remote add gmsl $(GSMLREPO)
+	$(GIT) subtree pull --prefix=$(GMSLDIR) --squash gmsl master
+	-$(GIT) remote remove gmsl
