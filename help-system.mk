3a1c7aaaf072b09b4a742cc4f5fdd3741412a352
sergey.s.betke <sergey.s.betke@yandex.ru>
2018-07-23 22:14:09 +0300

    * документированы цели * добавлена возможность документирования целей в подпроектах (IT-Service/ITG.MakeUtils#17)

--- help-system.mk
+++ help-system.mk
@@ -8,11 +8,15 @@ MAKE_HELP_SYSTEM_DIR = $(ITG_MAKEUTILS_DIR)
 need-help := $(filter help,$(MAKECMDGOALS))
 
 .PHONY: help
-help: ;
+help:: ;
 	$(if $(need-help),,$(info Type '$(MAKE) help' to get help.))
 
 define print-help
 $(if $(need-help),$(info $1 -- $2))
 endef
+
+ifndef SUBPROJECT
+_itg_makeutils_print-help = print-help
+endif
 
 endif
