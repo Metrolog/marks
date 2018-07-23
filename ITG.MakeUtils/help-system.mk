b293fd12639bd1813700e3a68272888ad1ab85d9
sergey.s.betke <sergey.s.betke@yandex.ru>
2018-07-23 21:15:52 +0300

    * добавлена заготовка с help-system.mk (IT-Service/ITG.MakeUtils#17)

---
+++ ITG.MakeUtils/help-system.mk
@@ -0,0 +1,18 @@
+ifndef ITG_MAKEUTILS_LOADED
+$(error 'ITG.MakeUtils/common.mk' must be included before any ITG.MakeUtils files.)
+endif
+
+ifndef MAKE_HELP_SYSTEM_DIR
+MAKE_HELP_SYSTEM_DIR = $(ITG_MAKEUTILS_DIR)
+
+need-help := $(filter help,$(MAKECMDGOALS))
+
+.PHONY: help
+help: ;
+	$(if $(need-help),,$(info Type '$(MAKE) help' to get help.))
+
+define print-help
+$(if $(need-help),$(info $1 -- $2))
+endef
+
+endif
