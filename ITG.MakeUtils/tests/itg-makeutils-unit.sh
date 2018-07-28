15ea167044a79605319fd9cdce7a9b362adfb06c
Sergey S. Betke <sergey.s.betke@yandex.ru>
2018-07-29 02:07:19 +0300

    * очередная заготовка обёртки тестов на bash (IT-Service/ITG.MakeUtils#18) [ci skip]

--- ITG.MakeUtils/tests/itg-makeutils-unit.sh
+++ ITG.MakeUtils/tests/itg-makeutils-unit.sh
@@ -1,64 +1,81 @@
 #!/bin/sh
 
-# enable debug mode
-if [ "$DEBUG" = "yes" ]; then
-	set -x
-fi
+export POSIXLY_CORRECT=1
 
-THIS_SCRIPT="$0"
-MAKE_TESTS_DIR=$(dirname "$0")
-MAKE_COMMON_DIR=$(dirname "$MAKE_TESTS_DIR")
+readonly THIS_SCRIPT="$0"
+readonly THIS_SCRIPT_FILENAME=$(basename "$THIS_SCRIPT")
+readonly MAKE_TESTS_DIR=$(dirname "$0")
+readonly MAKE_COMMON_DIR=$(dirname "$MAKE_TESTS_DIR")
 
-main() {
+# shellcheck source=../shflags/shflags
+. "$MAKE_COMMON_DIR/shflags/shflags"
+FLAGS_PARENT="$THIS_SCRIPT_FILENAME"
+
+DEFINE_string test_id '' 'test id (slug)'
+
+
+default_on_test_creation() {
+
 	if [ $# -lt 1 ]; then
-		"$THIS_SCRIPT" --help
+		( default_on_test_creation --help )
 		exit 1
 	fi
 
-	export POSIXLY_CORRECT=1
-
-	# shellcheck source=../shflags/shflags
-	. "$MAKE_COMMON_DIR/shflags/shflags"
-	FLAGS_PARENT="git flow"
-
-	DEFINE_boolean show_commands false 'show actions taken (git commands)' g
-
+	FLAGS_PARENT="default_on_test_creation"
 	FLAGS "$@" || exit $?
 	eval set -- "${FLAGS_ARGV}"
 
+	exit 0
+
+}
 
 
-	# sanity checks
-	SUBCOMMAND="$1"; shift
+default_on_test_change() {
 
-	if [ ! -e "$GITFLOW_DIR/git-flow-$SUBCOMMAND" ]; then
-		usage
+	if [ $# -lt 1 ]; then
+		( default_on_test_change --help )
 		exit 1
 	fi
 
-	# run command
-	. "$GITFLOW_DIR/git-flow-$SUBCOMMAND"
-	FLAGS_PARENT="git flow $SUBCOMMAND"
+	FLAGS_PARENT="default_on_test_change"
+	DEFINE_string test_status 'None' 'test status (None, Running, Passed, Failed, Ignored, Skipped, Inconclusive, NotFound, Cancelled, NotRunnable)'
+	DEFINE_integer test_exit_code 0 'test exit code'
+	FLAGS "$@" || exit $?
+	eval set -- "${FLAGS_ARGV}"
 
-	# test if the first argument is a flag (i.e. starts with '-')
-	# in that case, we interpret this arg as a flag for the default
-	# command
-	SUBACTION="default"
-	if [ "$1" != "" ] && { ! echo "$1" | grep -q "^-"; } then
-		SUBACTION="$1"; shift
-	fi
-	if ! type "cmd_$SUBACTION" >/dev/null 2>&1; then
-		warn "Unknown subcommand: '$SUBACTION'"
-		usage
+	set -o errexit
+	echo "Test \"${FLAGS_test_id:?}\" is ${FLAGS_test_status:?}."
+
+	exit 0
+
+}
+
+
+main() {
+
+	if [ $# -lt 1 ]; then
+		( main --help )
 		exit 1
 	fi
 
-	# run the specified action
-	if [ "$SUBACTION" != "help" ] && [ "$SUBCOMMAND" != "init" ] ; then
-    	init
-	fi
-	"cmd_$SUBACTION" "$@"
+	FLAGS_PARENT="$THIS_SCRIPT_FILENAME"
+	DEFINE_string on_test_add 'default_on_test_creation' 'test creation event handler'
+	DEFINE_string on_test_status_change 'default_on_test_change' 'tests events handler'
+	FLAGS "$@" || exit $?
+	eval set -- "${FLAGS_ARGV}"
 
+	echo '==============================================================================='
+	( "${FLAGS_on_test_add:?}" --test_id "${FLAGS_test_id:?}" ) || echo "Error in \"on_test_add\" event handler"
+	( "${FLAGS_on_test_status_change:?}" --test_id "${FLAGS_test_id:?}" --test_status Running ) || { echo "Error in \"on_test_status_change\" event handler"; }
+	echo "$@"
+	if ( "$@"; ); then
+		( "${FLAGS_on_test_status_change:?}" --test_id "${FLAGS_test_id:?}" --test_status Passed ) || { echo "Error in \"on_test_status_change\" event handler"; }
+	else
+		( "${FLAGS_on_test_status_change:?}" --test_id "${FLAGS_test_id:?}" --test_status Failed --test_exit_code $? ) || { "echo Error in \"on_test_status_change\" event handler"; }
+	fi
+	echo '==============================================================================='
+
+	exit 0
 
 }
 
