3e60243b67c32b089259742213e95865aa6e8408
sergey.s.betke <sergey.s.betke@yandex.ru>
2018-08-22 10:52:20 +0300

    * добавлен multifile тест (#69) [ci skip]

---
+++ stamps/tests/mf/mfTest.mk
@@ -0,0 +1,42 @@
+#!/usr/bin/make
+
+ifndef __itg_stamps_included
+$(error Do not run this file directly. Run 'Makefile' in root project folder.)
+endif
+
+THIS_MAKEFILE := $(lastword $(MAKEFILE_LIST))
+
+MFSTAMPSDPI := 1200
+
+$(eval $(call create_stamps, \
+  2018, \
+  $(call months,$(call range,1,12)) $(call quarters,$(call range,1,4)) $(call year), \
+  СП, \
+  $(call range,1,2), \
+  18 mm, \
+  false, \
+  verification_stamp, \
+  $(THIS_MAKEFILE) \
+))
+
+$(eval $(call create_stamps, \
+  2018, \
+  $(call months,1), \
+  СП, \
+  10, \
+  18 mm, \
+  false, \
+  verification_stamp_rhombus, \
+  $(THIS_MAKEFILE) \
+))
+
+$(eval $(call create_stamps, \
+  2018, \
+  $(call months,$(call range,1,2)), \
+  СП, \
+  $(call range,3,5), \
+  18 mm, \
+  false, \
+  calibration_stamp, \
+  $(THIS_MAKEFILE) \
+))
